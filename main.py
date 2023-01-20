ff6e3f9cb48ac32f79b4fad9e424977b4605cd35
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2023-01-20 16:45:32 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+How to convert dictionaries
+"""
+import pandas as pd
+import os
+import json
+import re
+
+
+path = os.getcwd()+"/S18items.xlsx"
+
+df = pd.read_excel(path)
+item_list = df.to_dict(orient="index")
+# item_list2 = df.set_index("Type").to_dict(orient="records")
+
+
+def save_file(dictionary, file_name):
+    ''' Function that converts python dictionary into javascript dictionary '''
+    json_str = json.dumps(dictionary, indent=4, ensure_ascii=False)
+    remove_quotes = re.sub("\"([^\"]+)\":", r"\1:", json_str)
+    file_name_cleaned = file_name.split(" ")[0]
+
+    with open(file_name_cleaned + ".js", "w", encoding='utf_8') as outfile:
+        outfile.write("export const " + file_name_cleaned +
+                      " = " + remove_quotes + ";")
+
+
+save_file(item_list, "test")
